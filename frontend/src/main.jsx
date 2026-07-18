25cacfbc8e2f8995eff3b86bfa9b7ddc4704639a
Garden-of-nedE <mal.anna824@gmail.com>
2026-07-18 20:47:44 +1000

    fix: typo in client.js that prevented proper redirection

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserReact } from 'react-router-dom'
-import App from '.App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
