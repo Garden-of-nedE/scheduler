b50660389569333c7e30b9c151905290e6163703
Garden-of-nedE <mal.anna824@gmail.com>
2026-07-18 19:50:51 +1000

    feat: frontend template created

---
+++ frontend/src/api/client.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const API_BASE_URL = import.meta.evn.VITE_APPPI_URL || 'http://localhost:8000'
+
+const client = axios.create({
+    baseURL: API_BASE_URL,
+})
+
+client.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+        config.headers.Authorization = 'Bearer ${token}'
+    }
+
+    return config
+}) 
+
+export default client
