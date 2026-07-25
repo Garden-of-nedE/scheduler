9685b0148c8d29d5b4fcae868ae86cccd89641d5
Garden-of-nedE <mal.anna824@gmail.com>
2026-07-25 21:27:02 +1000

    chore: updated UI icons

--- frontend/src/components/icons/Icons.jsx
+++ frontend/src/components/icons/Icons.jsx
@@ -118,3 +118,19 @@ export function RemoveIcon({ size = 24, ...props }) {
         </svg>
     )
 }
+
+export function NextIcon({ size = 24, ...props }) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" height={size} width={size} viewBox="0 -960 960 960" fill="currentColor">
+            <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/>
+            </svg>
+    )
+}
+
+export function PrevIcon({ size = 24, ...props}) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" height={size} width={size} viewBox="0 -960 960 960"  fill="currentColor">
+            <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z"/>
+            </svg>
+    )
+}
